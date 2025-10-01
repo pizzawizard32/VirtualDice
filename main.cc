fbce181d03968a2058e01ce5b9e65546723359cf
Giulio Tagariello <giuliomarianotagariello@gmail.com>
2025-10-01 16:33:39 +0200

    Update main.cc
    Added custom number limit and removed ratings, this is VirtualDIce 1.1.0 btw.

--- main.cc
+++ main.cc
@@ -4,30 +4,24 @@
 #include <string>
 #include "color.hpp"
 
-int max = 21;
-
 int main() {
-	std::string rating;
+	int max;
+	std::cout << "Set the number limit here: ";
+	std::cin >> max;
+	std::cout << "\n";
+
+	int rmax = max+1;
 	std::srand(std::time({}));
-	int random = std::rand() % max++;
+	int random = std::rand() % rmax;
 
 	if (random == 0) {
 		random++; // btw this is js incrementing to 1 if the result is 0
-		rating = "major skill issue";
-	} else if (random <= 3) {
-		rating = "major skill issue";
-	} else if (random <= 6) {
-		rating = "skill issue";
-	} else if (random <= 20) {
-		rating = "good";
-	} else if (random == 21) {
-		rating = "9+10";
 	}
 
 	std::cout << "@@@" << dye::light_red("######") << " Virtual" << dye::light_red("Dice") << " results\n";
 	std::cout << "@@@" << dye::light_red("######") << " -------------------\n";
 	std::cout << dye::light_red("###") << "@@@" << dye::light_red("###") << dye::light_yellow(" Score") << ": " << random << "\n";
-	std::cout << dye::light_red("###") << "@@@" << dye::light_red("###") << dye::light_yellow(" Rating") << ": " << rating << "\n";
+	std::cout << dye::light_red("###") << "@@@" << dye::light_red("###") << dye::light_yellow(" Maximum score") << ": " << max << "\n";
 	std::cout << dye::light_red("######") << "@@@\n";
 	std::cout << dye::light_red("######") << "@@@";
 
